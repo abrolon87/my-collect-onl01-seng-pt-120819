2a488d025ea890cf504dc1c0d6dcb81ca2e2cbde
Amanda Rolon <abrolon87@gmail.com>
2019-12-15 01:19:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,8 +6,14 @@
 #   end
 #   collection
 # end
-
-
+def my_collect(students)
+  s = 0
+  while s < students.length
+    mod_students << yield(students[s])
+    s += 1
+  end
+  mod_students
+end
 #hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
 my_collect(students) do |student|
   student.split(" ").first
